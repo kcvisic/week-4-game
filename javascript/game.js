3bcc297f73fc4656c470c79559d5c8643a8f4db1
kcvisic <kcvisic@gmail.com>
2017-08-26 21:59:23 -0400

    Progress

---
+++ javascript/game.js
@@ -0,0 +1,15 @@
+let DEFAULT_VALUE = 0;
+
+let generateRandomNumber = function(min, max) {
+	console.log("Hello, I generate a random number");
+}
+
+var GemGame = {
+    matchThisNumber: DEFAULT_VALUE,
+    wins: DEFAULT_VALUE,
+    losses: DEFAULT_VALUE,
+    gems: [DEFAULT_VALUE, DEFAULT_VALUE, DEFAULT_VALUE, DEFAULT_VALUE],
+    score: DEFAULT_VALUE,
+    generateRandomNumber: generateRandomNumber
+}
+
